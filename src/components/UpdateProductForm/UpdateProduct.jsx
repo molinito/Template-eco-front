543e269803f17890a6c14b4fbdc4ff7efe6a1ea5
Marcelo Saravia <molinito48@gmail.com>
2023-10-07 23:45:44 -0300

    correccion

--- src/components/UpdateProductForm/UpdateProduct.jsx
+++ src/components/UpdateProductForm/UpdateProduct.jsx
@@ -196,7 +196,7 @@ function UpdateProduct() {
 
   const addBody = (
     <div className="modo" >
-      <h6>Add New Producto</h6>
+      <h6>Add Nuevo Producto</h6>
       <FormControl  >
         <div >
           <TextField
